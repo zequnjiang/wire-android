5eaf21d898c8884cba0bacaa995fb19924e1b8f1
MohamadJaara <mohamad.jaara@wire.com>
2024-05-31 12:03:19 +0200

    fix: fdroid build script

--- docker-agent/builder.sh
+++ docker-agent/builder.sh
@@ -32,16 +32,15 @@ fi
 
 if [ "$BUILD_CLIENT" = true ] ; then
     echo "Compiling the client with Flavor:${CUSTOM_FLAVOR} and BuildType:${BUILD_TYPE}"
-    #./gradlew ${buildOption}assemble${FLAVOR_TYPE}${BUILD_TYPE}
-    ./gradlew ${buildOption}assemble${CUSTOM_FLAVOR}
+    ./gradlew ${buildOption}assemble${FLAVOR_TYPE}${BUILD_TYPE}
 else
     echo "Building the client will be skipped"
 fi
 
 if [ "$SIGN_APK" = true ] ; then
     echo "Signing APK with given details"
-    clientVersion=$(sed -ne "s/.*ANDROID_CLIENT_MAJOR_VERSION = \"\([^']*\)\"/\1/p" buildSrc/src/main/kotlin/Dependencies.kt)
-   /home/android-agent/android-sdk/build-tools/30.0.2/apksigner sign --ks ${HOME}/wire-android/${KEYSTORE_PATH} --ks-key-alias ${KEYSTORE_KEY_NAME} --ks-pass pass:${KSTOREPWD} --key-pass pass:${KEYPWD} "${HOME}/wire-android/app/build/outputs/apk/wire-${CUSTOM_FLAVOR,,}-${BUILD_TYPE,,}-${clientVersion}${PATCH_VERSION}.apk"
+    clientVersion=$(sed -ne "s/.*versionName = \"\([^']*\)\"/\1/p" build-logic/plugins/src/main/kotlin/AndroidCoordinates.kt)
+   /home/android-agent/android-sdk/build-tools/30.0.2/apksigner sign --ks ${HOME}/wire-android/${KEYSTORE_PATH} --ks-key-alias ${KEYSTORE_KEY_NAME} --ks-pass pass:${KSTOREPWD} --key-pass pass:${KEYPWD} "${HOME}/wire-android/app/build/outputs/apk/wire-${CUSTOM_FLAVOR,,}-${BUILD_TYPE,,}-${clientVersion}.apk"
 else
    echo "Apk will not be signed by the builder script"
 fi
