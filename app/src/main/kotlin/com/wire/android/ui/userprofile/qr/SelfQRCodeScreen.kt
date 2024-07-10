4f9abf0edf7bd28e878af6e2820b81f6375754a2
yamilmedina <yamilmedina@users.noreply.github.com>
2024-07-10 15:18:16 +0200

    feat: qr code screen share button

--- app/src/main/kotlin/com/wire/android/ui/userprofile/qr/SelfQRCodeScreen.kt
+++ app/src/main/kotlin/com/wire/android/ui/userprofile/qr/SelfQRCodeScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -31,10 +32,12 @@ import androidx.compose.material.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -47,6 +50,7 @@ import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.wire.android.R
 import com.wire.android.navigation.Navigator
 import com.wire.android.navigation.style.PopUpNavigationAnimation
+import com.wire.android.ui.common.button.WirePrimaryButton
 import com.wire.android.ui.common.colorsScheme
 import com.wire.android.ui.common.dimensions
 import com.wire.android.ui.common.scaffold.WireScaffold
@@ -157,9 +161,30 @@ private fun SelfQRCodeContent(
                 textAlign = TextAlign.Center,
                 color = colorsScheme().secondaryText
             )
+            Spacer(modifier = Modifier.weight(1f))
+            ShareLinkButton({/* TODO */ })
         }
     }
 
+}
+
+@Composable
+private fun ShareLinkButton(
+    onShareLinkClick: () -> Unit
+) {
+    WirePrimaryButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(dimensions().spacing16x)
+            .testTag("Share link"),
+        text = stringResource(R.string.user_profile_qr_code_share_link),
+        onClick = remember {
+            {
+                onShareLinkClick()
+            }
+        }
+    )
 }
 
 @PreviewMultipleThemes
